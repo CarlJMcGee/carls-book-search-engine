212cce1e32f030daaa07451a4b46a86cb1b6dacb
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-07-18 17:20:06 -0500

    added addUser and login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,30 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello World!";
+    me: async (parent, { username }) => User.findOne({ username: username }),
+    myselfnI: async () => User.find(),
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError(`Incorrect Credentials`);
+      }
+
+      const passVal = await user.isCorrectPassword(password);
+
+      if (!passVal) {
+        throw new AuthenticationError(`Incorrect Credentials`);
+      }
+
+      return user;
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,33 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type Books {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Books]
+  }
+  type Auth {
+    token: String
+    user: User
+  }
   type Query {
-    helloWorld: String
+    me(username: String!): User
+    myselfnI: [User]
+  }
+  type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
   }
 `;
 
