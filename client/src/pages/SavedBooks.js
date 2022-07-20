0f8d962e0fae5c092d9bf22ab9e8a0e72bb4e9e9
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-07-20 16:41:40 -0500

    replaced useEffect and userdata state

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -14,39 +14,8 @@ import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
   const [dataError, setError] = useState(null);
   const { loading, error, data } = useQuery(GET_ME);
-
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        if (error) {
-          setError(error);
-        }
-
-        if (!loading) {
-          if (data) {
-            const { me } = data;
-            setUserData(me);
-          }
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -64,7 +33,7 @@ const SavedBooks = () => {
       }
 
       const updatedUser = await response.json();
-      setUserData(updatedUser);
+      // setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -73,7 +42,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
@@ -81,6 +50,7 @@ const SavedBooks = () => {
   if (dataError) {
     return <h2>Unable to Load Saved Books</h2>;
   }
+  const userData = data.me;
 
   return (
     <>
